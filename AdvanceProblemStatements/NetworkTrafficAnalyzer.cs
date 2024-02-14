b38bb8990b4f47e4c69b33141cb947ad214add4d
Lokeshgithubcodes <ramisettylokesh642@gmail.com>
2024-02-14 14:34:52 +0530

    1st commit

---
+++ AdvanceProblemStatements/NetworkTrafficAnalyzer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceProblemStatements
+{
+    public class NetworkTrafficAnalyzer
+    {
+       
+        public void Analyze()
+        {
+
+            string filepath = @"C:\Users\Ramisettylokesh\OneDrive\Desktop\IPAddress.txt";
+
+            if (File.Exists(filepath))
+            {
+                try
+                {
+                    List<string> l = new List<string>();
+
+                    using (StreamReader sr = new StreamReader(filepath))
+                    {
+
+
+                        while (!sr.EndOfStream)
+                        {
+                            string content = sr.ReadLine();
+                            l.Add(content);
+                        }
+                    }
+                    Dictionary<string, int> d = new Dictionary<string, int>();
+                    foreach (string i in l)
+                    {
+                        if (d.ContainsKey(i))
+                        {
+                            d[i]++;
+                        }
+                        else
+                        {
+                            d[i] = 1;
+                        }
+                    }
+
+                    foreach (var k in d)
+                    {
+                        Console.WriteLine(k.Key + ": " + k.Value);
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else
+                Console.WriteLine("file not exsist");
+        }
+
+    }
+}
---
+++ AdvanceProblemStatements/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace AdvanceProblemStatements
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //----NetworkAnalyzer Project-----
+
+            NetworkTrafficAnalyzer nta =new NetworkTrafficAnalyzer();
+            nta.Analyze();
+
+
+
+
+
+        }
+
+    }
+}
